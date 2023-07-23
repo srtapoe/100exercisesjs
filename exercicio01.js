6995c9a7d1e3be749fc44485afa0cf799bed195f
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2023-07-23 13:28:55 -0300

    Adicionada solução parcial do exercício 1

--- exercicio01.js
+++ exercicio01.js
@@ -2,17 +2,34 @@ const prompt = require('prompt-sync')();
 
 let numero1 = parseInt(prompt('Digite o primeiro número: '));
 let numero2 = parseInt(prompt('Digite o segundo número: '));
-let operacaoSelecionada = 0;
+let operacaoSelecionada;
+let resultado;
 console.log("==================================================")
 
-function mostrarMenu(operacaoSelecionada) {
+
    console.log("===OPÇÕES DE OPERAÇÕES===")
     console.log("[1] - Adição")
     console.log("[2] - Subratração")
     console.log("[3] - Multiplicação")
     console.log("[4] - Divisão")  
     operacaoSelecionada = parseInt(prompt('Qual operação desejada?  '));
+
+
+
+function mostrarResultado(operacaoSelecionada, numero1, numero2, resultado) {
+  
+    if(operacaoSelecionada === 1){
+        resultado = numero1 + numero2
+    }else if(operacaoSelecionada === 2){
+        resultado = numero1 - numero2
+    }else if(operacaoSelecionada === 3){
+        resultado = numero1 * numero2
+    }else if(operacaoSelecionada === 4){
+        resultado = numero1 / numero2
+    }
+
+    console.log("Resultado: " +  resultado);
 }
 
 
-mostrarMenu(operacaoSelecionada)
+mostrarResultado(operacaoSelecionada, numero1, numero2);
